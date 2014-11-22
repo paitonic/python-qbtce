7696b88d4de19467c079efe72adddc25e850c9d8
tranq <edoomik@gmail.com>
2014-11-22 11:43:51 +0200

    fixed some pep8 errors

--- qbtce.py
+++ qbtce.py
@@ -14,6 +14,7 @@ import hashlib
 import hmac
 import sys
 
+
 def t():
     """Returns current date.
     """
@@ -26,16 +27,18 @@ def retry_connection(f):
     def retry(*args, **kwargs):
         seconds_to_retry = 5
         success = False
-        while (success == False):
+        while (not success):
             try:
                 result = f(*args, **kwargs)
                 success = True
                 return result
             except:
+                msg = "{0}: {1} --> connection problems . retry in {2} seconds."
                 print "{0}: {1} --> connection problems . retry in {2} seconds.".format(t(), f.__name__, seconds_to_retry)
                 time.sleep(seconds_to_retry)
         # return None
     return retry
+
 
 class Q:
     """BTC-E API class.
@@ -63,15 +66,13 @@ class Q:
     q.ticker('btc_usd') # btc/usd ticker
 
     """
-
-
     # set default key and secret
     __default_key = ""
     __default_secret = ""
 
     # TODO: set nonce_prefix to last N letters of key/secret?
     def __init__(self, key=__default_key, secret=__default_secret, nonce_prefix=''):
-        """Constructor 
+        """Constructor
 
         Initialize properties of 'Connection' instance.
         Keyword parameters:
@@ -85,7 +86,7 @@ class Q:
         self.BTCE_API_KEY = key
         self.BTCE_API_SECRET = secret
 
-        self.nonce_storage = str(nonce_prefix) + "_nonce" # storage file
+        self.nonce_storage = str(nonce_prefix) + "_nonce"  # storage file
         self.nonce = self.__load_nonce()
 
     def __save_nonce(self):
@@ -98,7 +99,7 @@ class Q:
             f.close()
         except:
             print '{0}: failed saving nonce value.'.format(t())
-        
+
     def __incr_nonce(self):
         """Increment the nonce value.
             Define your own algorithm if you wish.
@@ -106,7 +107,7 @@ class Q:
         self.nonce += 1
         self.__save_nonce()
         return self.nonce
-    
+
     def __load_nonce(self):
         """Load nonce value from storage self.nonce_storage) and assign it to self.nonce."""
         try:
@@ -125,10 +126,10 @@ class Q:
         pair            str     pair upon the method is called on, i.e. 'btc_usd'
 
         Returns:
-        Name        Type    				Desc
-        api_json    list of json(dict)    	response from the server
+        Name        Type                    Desc
+        api_json    list of json(dict)        response from the server
         """
-        
+
         request_url = self.public_base_url + pair + '/' + method_name
 
         connection = retry_connection(urllib.urlopen)
@@ -143,7 +144,6 @@ class Q:
 
         return api_json
 
-
     def __trade_query(self, method_name, method_params):
         """Calls the trade API methods (auth-required): getInfo, TransHistory, TradeHistory, ActiveOrders, Trade, CancelOrder
         returns response as list of json's.
@@ -154,24 +154,22 @@ class Q:
         method_params   dict    method parameters, each method have different args!
 
         Returns:
-        Name            Type    				Desc
+        Name            Type                    Desc
         api_json        list of json(dict)    response from the server
         """
         params = {'nonce': self.__incr_nonce(), 'method': method_name}
-        
+
         # if parameters supplied, add them to the existing parameters.
         if method_params != {}:
             for e in method_params.keys():
                 params[e] = method_params[e]
-        
+
         # signing parameters with HMAC-SHA-512
         params = urllib.urlencode(params)
         sha512_hash = hmac.new(self.BTCE_API_SECRET, digestmod=hashlib.sha512)
         sha512_hash.update(params)
         sign = sha512_hash.hexdigest()
-        headers = {"Content-type": "application/x-www-form-urlencoded",
-                       "Key":self.BTCE_API_KEY,
-                       "Sign":sign}
+        headers = {"Content-type": "application/x-www-form-urlencoded", "Key": self.BTCE_API_KEY, "Sign": sign}
         conn = httplib.HTTPSConnection("btc-e.com")
 
         connection = retry_connection(conn.request)
@@ -184,59 +182,59 @@ class Q:
             print "{0}: response status: {1} reason: {2}".format(t(), response.status, response.reason)
         api_json = json.load(response)
         conn.close()
-        
+
         # bad request handling
         if 'error' in api_json and str(api_json).find('invalid nonce') != -1:
             self.__incr_nonce()
-        elif 'error' in api_json: # other error
+        elif 'error' in api_json:  # other error
             print "{0} an error has been occured.\napi_json: {1}".format(t(), api_json)
         return api_json
 
     # --- basic public methods --- #
     def fee(self, pair):
-		"""Returns fee for requested pair.
+        """Returns fee for requested pair.
         parameters:
         Name    Type    Desc
         pair    str     valid pair like 'btc_usd', 'ltc_usd' etc.
 
         Returns:
-        Name    Type    			Desc
+        Name    Type                Desc
         -       list of json(dict)    fee
         """
         return self.__public_query('fee', pair)
 
     def ticker(self, pair):
-		"""Returns ticker for requested pair.
+        """Returns ticker for requested pair.
         parameters:
         Name    Type    Desc
         pair    str     valid pair like 'btc_usd', 'ltc_usd' etc.
 
         Returns:
-        Name    Type    		Desc
+        Name    Type            Desc
         -       list of json(dict)    ticker
         """
         return self.__public_query('ticker', pair)
 
     def trades(self, pair):
-		"""Returns trades for requested pair.
+        """Returns trades for requested pair.
         parameters:
         Name    Type    Desc
         pair    str     valid pair like 'btc_usd', ltc_usd' etc.
 
         Returns:
-        Name    Type    		Desc
+        Name    Type            Desc
         -       list of json(dict)    trades
         """
         return self.__public_query('trades', pair)
 
     def depth(self, pair):
-		"""Returns depth for requested pair.
+        """Returns depth for requested pair.
         Parameters:
         Name    Type    Desc
         pair    str     valid pair like 'btc_usd', ltc_usd' etc.
 
         Returns:
-        Name    Type    		Desc
+        Name    Type            Desc
         -       list of json(dict)    depth
         """
         return self.__public_query('depth', pair)
@@ -251,8 +249,8 @@ class Q:
         -- server time
 
         Parameters: None
-		Returns:
-        Name    Type    			Desc
+        Returns:
+        Name    Type                Desc
         -       list of json(dict)  various information about the account.
         """
         return self.__trade_query('getInfo', {})
@@ -261,11 +259,11 @@ class Q:
         """Returns history of transactions
         Parameters:
         Name    Type    Desc
-        params  dict    keys: from, count, from_id, 
+        params  dict    keys: from, count, from_id,
                         end_id, order, since, end
 
-		Returns:
-        Name    Type    			Desc
+        Returns:
+        Name    Type                Desc
         -       list of json(dict)  transaction history.
         """
         return self.conn.trade_query('TransHistory', params)
@@ -274,23 +272,23 @@ class Q:
         """Returns history of orders
         Parameters:
         Name    Type    Desc
-        params  dict    allowed keys: 'from', 'count', 'from_id', 
+        params  dict    allowed keys: 'from', 'count', 'from_id',
                         'end_id', 'order', 'since', 'end', 'pair'
 
-		Returns:
-        Name    Type    			Desc
-        -       list of json(dict) 	יhistory of orders.
+        Returns:
+        Name    Type                Desc
+        -       list of json(dict)     יhistory of orders.
         """
         return self.__trade_query('TradeHistory', params)
 
     def ActiveOrders(self, params):
         """Returns active orders
         Parameters:
-        Name    Type    Desc			
+        Name    Type    Desc
         params  dict    allowed keys: 'pair'. i.e {'pair': 'ltc_usd'}
 
-		Returns:
-        Name    Type    			Desc
+        Returns:
+        Name    Type                Desc
         -       list of json(dict)  active orders
         """
         return self.__trade_query('ActiveOrders', params)
@@ -301,8 +299,8 @@ class Q:
         Name    Type    Desc
         params  dict    allowed keys: 'pair', 'type', 'rate', 'amount'
 
-		Returns:
-        Name    Type    			Desc
+        Returns:
+        Name    Type                Desc
         -       list of json(dict)  returns funds, remains, received and more.
         """
         return self.__trade_query('Trade', params)
@@ -313,8 +311,8 @@ class Q:
         Name    Type    Desc
         params  dict    keys: order_id
 
-		Returns:
-        Name    Type    			Desc
+        Returns:
+        Name    Type                Desc
         -       list of json(dict)  return order_id, funds.
         """
         return self.__trade_query('CancelOrder', params)
